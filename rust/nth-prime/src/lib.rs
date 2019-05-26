27d5301c89c1bf1b06f0fee7913f95d50b3d87c2
Luke Carrier <luke@carrier.im>
2019-05-26 23:13:54 +0100

    rust/nth-prime: ignore evens, too
    If we specifically handle the case where n == 2 we can ignore all other
    even numbers since these are all divisible by 2.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -14,10 +14,15 @@ pub fn is_prime(n: u32) -> bool {
     if n == 1 {
         // 1 is the unit; it's not prime
         return false;
+    } else if n == 2 {
+        return true;
+    } else if n > 2 && n % 2 == 0 {
+        // Divisible by 2
+        return false;
     }
 
     let max_divisor = (n as f32).sqrt().floor() as u32;
-    for divisor in 2..=max_divisor {
+    for divisor in (3..=max_divisor).step_by(2) {
         if n % divisor == 0 {
             return false;
         }
