ec993566b7034f0dca5f35cd356e56ec88076de7
Luke Carrier <luke@carrier.im>
2019-05-26 23:11:13 +0100

    rust/nth-prime: rough solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes = Vec::new();
+    let mut i = 2;
+    while primes.len() != (n as usize) + 1 {
+        if is_prime(i) {
+            primes.push(i)
+        }
+        i += 1;
+    }
+    *primes.last().expect("we ain't got no primes")
+}
+
+pub fn is_prime(n: u32) -> bool {
+    if n == 1 {
+        // 1 is the unit; it's not prime
+        return false;
+    }
+
+    for divisor in 2..n {
+        if n % divisor == 0 {
+            return false;
+        }
+    }
+
+    true
 }
