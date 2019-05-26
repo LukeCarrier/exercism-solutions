6ae10e6e9ca1cf9f3c76fdd1d7b486876db808d5
Luke Carrier <luke@carrier.im>
2019-05-27 00:03:53 +0100

    rust/nth-prime: make it more rustworthy

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,32 +1,22 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes = Vec::new();
-    let mut i = 2;
-    while primes.len() != (n as usize) + 1 {
-        if is_prime(i) {
-            primes.push(i)
-        }
-        i += 1;
-    }
-    *primes.last().expect("we ain't got no primes")
+    (2..)
+        .filter(|&i| is_prime(i))
+        .nth(n as usize)
+        .expect("we ain't got no primes")
 }
 
 pub fn is_prime(n: u32) -> bool {
-    if n == 1 {
-        // 1 is the unit; it's not prime
-        return false;
-    } else if n == 2 {
-        return true;
-    } else if n > 2 && n % 2 == 0 {
-        // Divisible by 2
-        return false;
-    }
+    match n {
+        1 => false,
+        2 => true,
 
-    let max_divisor = (n as f32).sqrt().floor() as u32;
-    for divisor in (3..=max_divisor).step_by(2) {
-        if n % divisor == 0 {
-            return false;
-        }
-    }
+        _ if n % 2 == 0 => false,
 
-    true
+        _ => {
+            let max_divisor = (n as f32).sqrt().floor() as u32;
+            !(3..=max_divisor)
+                .step_by(2)
+                .any(|d| n % d == 0)
+        },
+    }
 }
