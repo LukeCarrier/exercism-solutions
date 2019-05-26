e0fb77ac4074aac7b6dd12a629fa8256fd8ee613
Luke Carrier <luke@carrier.im>
2019-05-26 22:31:33 +0100

    rust/raindrops: solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut parts = Vec::<String>::with_capacity(3);
+
+    if n % 3 == 0 {
+        parts.push("Pling".to_string());
+    }
+    if n % 5 == 0 {
+        parts.push("Plang".to_string());
+    }
+    if n % 7 == 0 {
+        parts.push("Plong".to_string());
+    }
+
+    if parts.is_empty() {
+        parts.push(n.to_string());
+    }
+
+    parts.join("")
 }
