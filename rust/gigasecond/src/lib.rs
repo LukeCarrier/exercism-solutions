ce8d9f9e889f66e9723f86548c12101730f768c7
Luke Carrier <luke@carrier.im>
2019-05-27 20:39:56 +0100

    rust/gigasecond: use literal, extract constant

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
 
+const GIGASECOND_SECS: i64 = 1_000_000_000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10i64.pow(9))
+    start + Duration::seconds(GIGASECOND_SECS)
 }
