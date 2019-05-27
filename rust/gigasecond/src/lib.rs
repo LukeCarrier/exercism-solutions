05026356a41221b54f60ce0a470d92f60c56689c
Luke Carrier <luke@carrier.im>
2019-05-27 15:29:38 +0100

    rust/gigasecond: simplify typing

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,5 +2,5 @@ use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10u64.pow(9) as i64)
+    start + Duration::seconds(10i64.pow(9))
 }
