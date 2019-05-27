a8e9a3a5ac658444203cb47e614c94c66859a589
Luke Carrier <luke@carrier.im>
2019-05-27 11:53:05 +0100

    rust/proverb: solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let num = list.len();
+    if num == 0 {
+        return String::new();
+    }
+
+    let mut result = Vec::<String>::with_capacity(num);
+    for i in 1..num {
+        result.push(format!(
+            "For want of a {} the {} was lost.\n",
+            list.get(i - 1).unwrap(),
+            list.get(i).unwrap()
+        ));
+    }
+    result.push(format!(
+        "And all for the want of a {}.",
+        list.first().unwrap()
+    ));
+    result.join("")
 }
