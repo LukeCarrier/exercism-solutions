a125255ab5d7c165f0d8204e897a4b91ee5a9538
Luke Carrier <luke@carrier.im>
2019-05-27 11:58:39 +0100

    rust/proverb: use iterators

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,20 +1,10 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let num = list.len();
-    if num == 0 {
-        return String::new();
+    if list.is_empty() {
+        String::new()
+    } else {
+        list.windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .fold(String::new(), |acc, v| acc + &v)
+            + &format!("And all for the want of a {}.", list[0])
     }
-
-    let mut result = Vec::<String>::with_capacity(num);
-    for i in 1..num {
-        result.push(format!(
-            "For want of a {} the {} was lost.\n",
-            list.get(i - 1).unwrap(),
-            list.get(i).unwrap()
-        ));
-    }
-    result.push(format!(
-        "And all for the want of a {}.",
-        list.first().unwrap()
-    ));
-    result.join("")
 }
