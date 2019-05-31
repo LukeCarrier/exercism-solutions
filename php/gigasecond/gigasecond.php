cb339e15ded0cfb1967600669854dbbd6a2c9e53
Luke Carrier <luke@carrier.im>
2019-05-31 22:00:40 +0100

    php/gigasecond: solve

---
+++ php/gigasecond/gigasecond.php
@@ -0,0 +1,9 @@
+<?php
+
+const GIGASECOND_SECS = 10**9;
+
+function from(DateTime $date): DateTime {
+    $interval = new DateInterval(sprintf('PT%dS', GIGASECOND_SECS));
+    $result = clone $date;
+    return $result->add($interval);
+}
