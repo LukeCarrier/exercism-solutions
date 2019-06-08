e3d0b4179f78c3a67fb260ee1fd066b83d038b2a
Luke Carrier <luke@carrier.im>
2019-06-08 15:51:40 +0100

    php/gigasecond: use style consistent with exercise

--- php/gigasecond/gigasecond.php
+++ php/gigasecond/gigasecond.php
@@ -2,7 +2,8 @@
 
 const GIGASECOND_SECS = 10**9;
 
-function from(DateTime $date): DateTime {
+function from(DateTime $date): DateTime
+{
     $interval = new DateInterval(sprintf('PT%dS', GIGASECOND_SECS));
     $result = clone $date;
     return $result->add($interval);
