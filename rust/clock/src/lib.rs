2800d6e0fd722553334d706f0fae0472ae8c3644
Luke Carrier <luke@carrier.im>
2019-06-08 17:59:19 +0100

    rust/clock: combine hours and minutes

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,40 +1,34 @@
 use std::fmt;
 
-const HOURS_IN_DAY: u32 = 24;
-const MINUTES_IN_HOUR: u32 = 60;
-const MINUTES_IN_DAY: u32 = HOURS_IN_DAY * MINUTES_IN_HOUR;
+const HOURS_IN_DAY: i32 = 24;
+const MINUTES_IN_HOUR: i32 = 60;
+const MINUTES_IN_DAY: i32 = HOURS_IN_DAY * MINUTES_IN_HOUR;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Clock {
-    pub hours: u32,
-    pub minutes: u32,
+    pub total_minutes: u32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         let mut total_minutes = hours * 60 + minutes;
         if total_minutes < 0 {
-            total_minutes =
-                    total_minutes % MINUTES_IN_DAY as i32
-                    + MINUTES_IN_DAY as i32;
+            total_minutes = total_minutes % MINUTES_IN_DAY + MINUTES_IN_DAY;
         }
+        total_minutes = total_minutes % MINUTES_IN_DAY;
 
-        let hours = (total_minutes / MINUTES_IN_HOUR as i32) % HOURS_IN_DAY as i32;
-        let minutes = total_minutes % MINUTES_IN_HOUR as i32;
-
-        Self {
-            hours: hours as u32,
-            minutes: minutes as u32,
-        }
+        Self { total_minutes: total_minutes as u32 }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self::new(self.hours as i32, self.minutes as i32 + minutes)
+        Self::new(0, self.total_minutes as i32 + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        let hours = self.total_minutes as i32 / MINUTES_IN_HOUR;
+        let minutes = self.total_minutes as i32 % MINUTES_IN_HOUR;
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
