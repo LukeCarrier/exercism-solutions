3a3e4e7cbe7ddba9ddd57a9be6a04ecb87faf87c
Luke Carrier <luke@carrier.im>
2019-06-08 22:11:56 +0100

    rust/matching-brackets: solve

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
+
+static OPEN_BRACKET: [char; 3] = ['[', '{', '('];
+static CLOSE_BRACKET: [char; 3] = [']', '}', ')'];
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut pairs = HashMap::new();
+    pairs.insert(OPEN_BRACKET[0], CLOSE_BRACKET[0]);
+    pairs.insert(OPEN_BRACKET[1], CLOSE_BRACKET[1]);
+    pairs.insert(OPEN_BRACKET[2], CLOSE_BRACKET[2]);
+
+    let brackets = string
+        .chars()
+        .filter(|c| OPEN_BRACKET.contains(c) || CLOSE_BRACKET.contains(c));
+
+    let mut stack = Vec::new();
+    for bracket in brackets {
+        if OPEN_BRACKET.contains(&bracket) {
+            stack.push(bracket);
+        } else if !stack.is_empty() && pairs[stack.last().unwrap()] == bracket {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    stack.is_empty()
 }
