4a31af9473d400d0cd7876881991505f58ef37c6
Luke Carrier <luke@carrier.im>
2019-06-09 13:35:52 +0100

    rust/matching-brackets: golf it a little

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,27 +1,17 @@
-use std::collections::HashMap;
-
-static OPEN_BRACKET: [char; 3] = ['[', '{', '('];
-static CLOSE_BRACKET: [char; 3] = [']', '}', ')'];
+const OPEN_BRACKET: &str = "[{(";
+const CLOSE_BRACKET: &str = "]})";
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut pairs = HashMap::new();
-    pairs.insert(OPEN_BRACKET[0], CLOSE_BRACKET[0]);
-    pairs.insert(OPEN_BRACKET[1], CLOSE_BRACKET[1]);
-    pairs.insert(OPEN_BRACKET[2], CLOSE_BRACKET[2]);
-
-    let brackets = string
-        .chars()
-        .filter(|c| OPEN_BRACKET.contains(c) || CLOSE_BRACKET.contains(c));
-
     let mut stack = Vec::new();
-    for bracket in brackets {
-        if OPEN_BRACKET.contains(&bracket) {
-            stack.push(bracket);
-        } else if !stack.is_empty() && pairs[stack.last().unwrap()] == bracket {
-            stack.pop();
-        } else {
-            return false;
-        }
-    }
-    stack.is_empty()
+    string
+        .chars()
+        .filter(|&c| OPEN_BRACKET.find(c).is_some() || CLOSE_BRACKET.find(c).is_some())
+        .all(|c| {
+            if let Some(open) = OPEN_BRACKET.find(c) {
+                stack.push(open);
+                true
+            } else {
+                stack.pop() == CLOSE_BRACKET.find(c)
+            }
+        }) && stack.is_empty()
 }
