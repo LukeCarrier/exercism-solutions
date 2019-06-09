6ad88bc126900121ce85917ad1736a64166aab1f
Luke Carrier <luke@carrier.im>
2019-06-09 14:41:20 +0100

    php/roman-numerals: handle subtractions

--- php/roman-numerals/roman-numerals.php
+++ php/roman-numerals/roman-numerals.php
@@ -2,27 +2,24 @@
 
 function toRoman(int $number): string
 {
-    $values = [
-        'I' => 1,
-        'V' => 5,
-        'X' => 10,
-        'L' => 50,
-        'C' => 100,
-        'D' => 500,
-        'M' => 1000,
+    $numerals = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
     ];
-    arsort($values);
 
-
-    // TODO: on repetitions of 4 we have to subtract the lower value from the larger one on the right
-    // IV = 4
-    // IX = 9
-    // XL = 40
-    // XC = 90
-    // CD = 400
-    // CM = 900
     $result = '';
-    foreach ($values as $letter => $value) {
+    foreach ($numerals as $value => $letter) {
         $count = $number / $value;
         $result .= str_repeat($letter, $count);
         $number = $number % $value;
