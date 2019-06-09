5430c0e8e0929a1f199b61d9edcbd07784fa2f60
Luke Carrier <luke@carrier.im>
2019-06-09 14:14:53 +0100

    .: let's get lazy

--- activate.sh
+++ activate.sh
@@ -5,11 +5,26 @@ _root="$(cd "$(dirname "$1")" && pwd -P)"
 alias exercism="${_root}/_exercism/exercism"
 
 if [ -n "$BASH_VERSION" ]; then
-    . "${_root}/_exercism/shell/exercism_completion.bash"
+  . "${_root}/_exercism/shell/exercism_completion.bash"
 elif [ -n "$ZSH_VERSION" ]; then
-    . "${_root}/_exercism/shell/exercism_completion.zsh"
+  . "${_root}/_exercism/shell/exercism_completion.zsh"
 fi
 
-unset _root
-
 exercism configure --workspace "$_root"
+
+solve() {
+  local track_exercise="$1"
+  local track="$(echo "$track_exercise" | cut -d / -f 1)"
+  local exercise="$(echo "$track_exercise" | cut -d / -f 2)"
+  local workspace="$(exercism workspace)"
+
+  if [ ! -d "$workspace/$track/$exercise" ]; then
+    rm -f "$workspace/$track/$exercise"
+    exercism download --exercise="$exercise" --track="$track"
+    git --work-tree "$workspace" add "$track/$exercise"
+    git --work-tree "$workspace" commit "$track/$exercise" -m "$track/$exercise: exercise"
+  fi
+  code "$track/$exercise"
+}
+
+unset _root
