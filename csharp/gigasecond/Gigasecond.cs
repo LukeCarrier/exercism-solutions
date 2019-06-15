a24dd614d3287e9f20f44bf7e9b21d8b13034449
Luke Carrier <luke@carrier.im>
2019-06-15 12:37:28 +0100

    csharp/gigasecond: use scientific notation

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,10 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    public static double GIGASECOND
-    {
-        get => Math.Pow(10, 9);
-    }
+    const double GIGASECOND = 1e9;
 
     public static DateTime Add(DateTime moment)
     {
