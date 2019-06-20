20ad77d9fb3b2513e629fdd921a09527bbb8cf45
Luke Carrier <luke@carrier.im>
2019-06-20 20:40:17 +0100

    csharp/space-age: solve

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,47 +2,59 @@ using System;
 
 public class SpaceAge
 {
+    const double EARTH_ORBIT_SECONDS = 31557600.0;
+    const double MERCURY_ORBIT_EARTH_YEARS = 0.2408467;
+    const double VENUS_ORBIT_EARTH_YEARS = 0.61519726;
+    const double MARS_ORBIT_EARTH_YEARS = 1.8808158;
+    const double JUPITER_ORBIT_EARTH_YEARS = 11.862615;
+    const double SATURN_ORBIT_EARTH_YEARS = 29.447498;
+    const double URANUS_ORBIT_EARTH_YEARS = 84.016846;
+    const double NEPTUNE_ORBIT_EARTH_YEARS = 164.79132;
+
+    private int seconds;
+
     public SpaceAge(int seconds)
     {
+        this.seconds = seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return seconds / EARTH_ORBIT_SECONDS;
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / MERCURY_ORBIT_EARTH_YEARS;
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / VENUS_ORBIT_EARTH_YEARS; 
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / MARS_ORBIT_EARTH_YEARS;
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / JUPITER_ORBIT_EARTH_YEARS;
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / SATURN_ORBIT_EARTH_YEARS;
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / URANUS_ORBIT_EARTH_YEARS;
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / NEPTUNE_ORBIT_EARTH_YEARS;
     }
 }
