0af52f900286d928546234855a222bc16a7e0313
Luke Carrier <luke@carrier.im>
2019-06-20 19:19:36 +0100

    ruby/matrix: cut out delimiters

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,13 +1,10 @@
 class Matrix
-  ROW_DELIMITER = "\n"
-  COLUMN_DELIMITER = " "
-
   attr_reader :rows, :columns
 
   def initialize(matrix)
     @values = []
-    matrix.split(ROW_DELIMITER).each do |row|
-      @values.append(row.split(COLUMN_DELIMITER).map { |v| v.to_i })
+    matrix.lines.each do |row|
+      @values.append(row.split.map { |v| v.to_i })
     end
   end
 
