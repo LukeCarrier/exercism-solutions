c054c507e12b042a7e255acbfeccf3975e3becb6
Luke Carrier <luke@carrier.im>
2019-06-22 18:49:06 +0100

    php/bob: further simplify

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -2,50 +2,53 @@
 
 class Bob
 {
-    const TYPE_SILENCE = 1;
-    const TYPE_QUESTION = 2;
-    const TYPE_STATEMENT = 3;
-
     const RESPONSE_SHOUTED_QUESTION = 'Calm down, I know what I\'m doing!';
     const RESPONSE_QUESTION = 'Sure.';
     const RESPONSE_SILENCE = 'Fine. Be that way!';
     const RESPONSE_SHOUTED_WILDCARD = 'Whoa, chill out!';
     const RESPONSE_WILDCARD = 'Whatever.';
 
-    const SILENCE_CHARS = "\n\r\t\0\x0B \u{000b}\u{00a0}\u{2002}";
+    const RE_QUESTION = '/\?$/';
+    const RE_SPACE = '/[ \n\r\t\v\0\p{Z}]/u';
+    const RE_LETTER = '/\p{L}/u';
+    const RE_LOWER_LETTER = '/\p{Ll}/u';
 
-    public static function typeFrom(string $message): int
+    public static function stripWhitespace($message)
     {
-        $message = trim($message, static::SILENCE_CHARS);
-        if (strlen($message) === 0) {
-            return static::TYPE_SILENCE;
-        } elseif (preg_match('/\?$/', $message) === 1) {
-            return static::TYPE_QUESTION;
-        } else {
-            return static::TYPE_STATEMENT;
-        }
+        return preg_replace(static::RE_SPACE, '', $message);
+    }
+
+    public static function isQuestion($message)
+    {
+        return preg_match(static::RE_QUESTION, $message) === 1;
+    }
+
+    public static function isSilence($message)
+    {
+        return strlen($message) === 0;
     }
 
     public static function isShouting(string $message): bool
     {
-        $hasLetters = preg_match('/\p{L}/u', $message);
-        $hasLowerLetters = preg_match('/\p{Ll}/u', $message);
+        $hasLetters = preg_match(static::RE_LETTER, $message);
+        $hasLowerLetters = preg_match(static::RE_LOWER_LETTER, $message);
 
         return $hasLetters && !$hasLowerLetters;
     }
 
     public function respondTo(string $message): string
     {
-        $messageType = static::typeFrom($message);
-        $shouted = static::isShouting($message);
+        $message = static::stripWhitespace($message);
 
-        switch ($messageType) {
-            case static::TYPE_QUESTION:
+        if (static::isSilence($message)) {
+            return static::RESPONSE_SILENCE;
+        } else {
+            $shouted = static::isShouting($message);
+            if (static::isQuestion($message)) {
                 return $shouted ? static::RESPONSE_SHOUTED_QUESTION : static::RESPONSE_QUESTION;
-            case static::TYPE_SILENCE:
-                return static::RESPONSE_SILENCE;
-            default:
+            } else {
                 return $shouted ? static::RESPONSE_SHOUTED_WILDCARD : static::RESPONSE_WILDCARD;
+            }
         }
     }
 }
