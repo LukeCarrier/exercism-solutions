08c7a6cfba7d9a52b6882d31afc408d7badee415
Luke Carrier <luke@carrier.im>
2019-06-23 15:54:47 +0100

    csharp/sum-of-multiples: solve

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        multiples = multiples.Where(m => m != 0);
+
+        return Enumerable
+            .Range(0, max)
+            .Where(i => multiples.Any(m => i % m == 0))
+            .Sum();
     }
 }
