c7fb5c9fbfe6c7150cc640b9c7fdfbc1285f8f32
Luke Carrier <luke@carrier.im>
2019-06-23 14:51:03 +0100

    bash/leap: solve

---
+++ bash/leap/leap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+shopt -s extglob
+
+if (( $# != 1 )) || [[ -n "${1##+([0-9])}" ]]; then
+  echo 'Usage: leap.sh <year>'
+  exit 1
+fi
+
+if (( $1 % 4 == 0 )) && ( (( $1 % 100 != 0 )) || (( $1 % 400 == 0 )) ); then
+  echo 'true'
+else
+  echo 'false'
+fi
