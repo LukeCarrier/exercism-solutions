0e22ab991d5d4cd3a725d999d5a64fff3fdd4427
Luke Carrier <luke@carrier.im>
2019-06-23 18:10:40 +0100

    rust/saddle-points: solve

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut saddle_points = Vec::new();
+    for (y, row) in input.iter().enumerate() {
+        for (x, value) in row.iter().enumerate() {
+            let lt_row = row.iter().any(|v| value < v);
+            let gt_col = (0..input.len()).any(|v| *value > input[v][x]);
+
+            if !(lt_row || gt_col) {
+                saddle_points.push((y, x));
+            }
+        }
+    }
+    saddle_points
 }
