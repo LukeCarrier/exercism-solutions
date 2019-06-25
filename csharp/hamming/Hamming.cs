9adcc470a93e333907d4d285889dc03635b3021b
Luke Carrier <luke@carrier.im>
2019-06-25 20:18:54 +0100

    csharp/hamming: solve

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length != secondStrand.Length) {
+            throw new ArgumentException("Left and right strands must be of equal length");
+        }
+
+        return firstStrand.ToCharArray().Zip(secondStrand.ToCharArray(), (s1, s2) => s1 == s2 ? 0 : 1).Sum();
     }
 }
