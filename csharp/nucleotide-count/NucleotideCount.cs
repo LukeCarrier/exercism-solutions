b9f263fd4c013ecfba0d91bdeabbdd98351a1722
Luke Carrier <luke@carrier.im>
2019-06-27 19:25:22 +0100

    csharp/nucleotide-count: solve

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,20 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IDictionary<char, int> result = new Dictionary<char, int>()
+        {
+            { 'A', 0 },
+            { 'C', 0 },
+            { 'G', 0 },
+            { 'T', 0 },
+        };
+        foreach (var nucleotide in sequence) {
+            try {
+                result[nucleotide]++;
+            } catch (KeyNotFoundException) {
+                throw new ArgumentException($"'{nucleotide}' doesn't represent a valid nucleotide");
+            }
+        }
+        return result;
     }
 }
