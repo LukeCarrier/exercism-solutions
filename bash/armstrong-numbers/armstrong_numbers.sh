790d97af704507d5b28de5ccafb2374b9800d4f8
Luke Carrier <luke@carrier.im>
2019-07-30 14:50:11 +0100

    bash/armstrong-numbers: check input's numeric

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if (( $# != 1 )) || ! [[ "$1" =~ ^[0-9]+$ ]]; then
+  echo "Usage: $0 <number>"
+  exit 1
+fi
+
 declare -i sum
 for (( i = 0; i < ${#1}; i++ )); do
   (( sum += ${1:$i:1} ** ${#1} ))
