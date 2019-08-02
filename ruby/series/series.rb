b9f30bcd25e952c8778d3ecbdb6ac76753e90bcf
Luke Carrier <luke@carrier.im>
2019-08-02 12:36:09 +0100

    ruby/series: drop to_a, use shorthand proc

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -5,6 +5,6 @@ class Series
 
   def slices(length)
     raise ArgumentError.new('Slice length cannot be > series length') unless @series.length >= length
-    @series.chars.each_cons(length).map { |v| v.join }.to_a
+    @series.chars.each_cons(length).map(&:join)
   end
 end
