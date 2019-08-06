c8191f3e57ec4819d6f779b56d460c62de6df98b
Luke Carrier <luke@carrier.im>
2019-08-06 21:06:40 +0100

    csharp/allergies: complete it

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -1,30 +1,39 @@
 using System;
+using System.Linq;
 
+[Flags]
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs         = 1 << 0,
+    Peanuts      = 1 << 1,
+    Shellfish    = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes     = 1 << 4,
+    Chocolate    = 1 << 5,
+    Pollen       = 1 << 6,
+    Cats         = 1 << 7,
 }
 
 public class Allergies
 {
-    public Allergies(int mask)
+    public Allergen Mask
     {
+        get;
+        protected set;
     }
 
-    public bool IsAllergicTo(Allergen allergen)
+    public Allergies(int mask)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Mask = (Allergen) mask;
     }
+
+    public bool IsAllergicTo(Allergen allergen) => Mask.HasFlag(allergen);
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enum.GetValues(typeof(Allergen))
+            .Cast<Allergen>()
+            .Where(allergen => IsAllergicTo(allergen))
+            .ToArray();
     }
 }
