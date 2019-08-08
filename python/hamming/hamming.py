4bb68efcf2b4437d92b1867513ebd040f9ac20d3
Luke Carrier <luke@carrier.im>
2019-08-08 21:17:54 +0100

    python/hamming: use list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,9 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Left and right strands must be of equal length")
 
-    distance = 0
-    for a, b in zip(list(strand_a), list(strand_b)):
-        if a != b:
-            distance += 1
-
-    return distance
+    return sum([a != b for a, b in zip(strand_a, strand_b)])
