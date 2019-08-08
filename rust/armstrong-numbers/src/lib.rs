2ce541f7fe5b30c43fd40e0ab4da79396242c7e5
Luke Carrier <luke@carrier.im>
2019-08-08 21:37:55 +0100

    rust/armstrong-numbers: solve

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let count = (num as f32).log10() as u32 + 1;
+    let mut sum = 0;
+    let mut work = num;
+
+    while work > 0 {
+        sum += (work % 10).pow(count);
+        work /= 10;
+    }
+    num == sum
 }
