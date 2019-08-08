79ed3d4a58af36c5e180aef04bd15ae0bfb955fe
Luke Carrier <luke@carrier.im>
2019-08-08 21:14:08 +0100

    python/hamming: solve

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Left and right strands must be of equal length")
+
+    distance = 0
+    for a, b in zip(list(strand_a), list(strand_b)):
+        if a != b:
+            distance += 1
+
+    return distance
