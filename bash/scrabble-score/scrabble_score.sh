62b68e72778a3017561d887494696a753946e9df
Luke Carrier <luke@carrier.im>
2019-08-09 23:42:20 +0100

    bash/scrabble-score: golf it a bit

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -3,21 +3,14 @@
 input="${1^^}"
 score=0
 for (( i=0; i<${#input}; i++ )); do
-  char="${input:$i:1}"
-  if [[ "$char" =~ [AEIOULNRST] ]]; then
-    (( score += 1 ))
-  elif [[ "$char" =~ [DG] ]]; then
-    (( score += 2 ))
-  elif [[ "$char" =~ [BCMP] ]]; then
-    (( score += 3 ))
-  elif [[ "$char" =~ [FHVWY] ]]; then
-    (( score += 4 ))
-  elif [[ "$char" == K ]]; then
-    (( score += 5 ))
-  elif [[ "$char" =~ [JX] ]]; then
-    (( score += 8 ))
-  elif [[ "$char" =~ [QZ] ]]; then
-    (( score += 10 ))
-  fi
+  case "${input:$i:1}" in
+    [AEIOULNRST]) (( score += 1  )) ;;
+    [DG])         (( score += 2  )) ;;
+    [BCMP])       (( score += 3  )) ;;
+    [FHVWY])      (( score += 4  )) ;;
+    K)            (( score += 5  )) ;;
+    [JX])         (( score += 8  )) ;;
+    [QZ])         (( score += 10 )) ;;
+  esac
 done
 echo $score
