021f08d66ce5f58944e685fceb19005453e14672
Luke Carrier <luke@carrier.im>
2019-08-09 23:04:30 +0100

    ruby/resistor-color-duo: golf it

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,19 +1,8 @@
 class ResistorColorDuo
-  COLORS = {
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9,
-  }
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(bands)
     first, second = bands
-    10 * COLORS[first] + COLORS[second]
+    10 * COLORS.index(first) + COLORS.index(second)
   end
 end
