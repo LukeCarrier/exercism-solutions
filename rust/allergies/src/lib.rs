f901bb0efff3408743046b99f45e68c2a8e35db7
Luke Carrier <luke@carrier.im>
2019-08-11 13:39:37 +0100

    rust/allergies: working implementation

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,33 +1,44 @@
-pub struct Allergies;
-
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs         = 1 << 0,
+    Peanuts      = 1 << 1,
+    Shellfish    = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes     = 1 << 4,
+    Chocolate    = 1 << 5,
+    Pollen       = 1 << 6,
+    Cats         = 1 << 7,
+}
+
+impl Allergen {
+    pub fn all() -> &'static [Self] {
+        &[
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+    }
+}
+
+pub struct Allergies {
+    pub score: u32,
 }
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        return Self { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        self.score & *allergen as u32 > 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Allergen::all().iter().filter(|a| self.is_allergic_to(a)).cloned().collect()
     }
 }
