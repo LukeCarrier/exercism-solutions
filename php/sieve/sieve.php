c8f3ca0b26c7b4d5721eb0785ff63b522d2bda25
Luke Carrier <luke@carrier.im>
2019-08-12 02:25:12 +0100

    php/sieve: solve

--- php/sieve/sieve.php
+++ php/sieve/sieve.php
@@ -1,1 +1,21 @@
 <?php
+
+function sieve(int $limit)
+{
+    if ($limit < 2) {
+        return [];
+    }
+
+    $number = 2;
+    $range = range($number, $limit);
+    $primes = array_combine($range, $range);
+
+    while ($number ** 2 <= $limit) {
+        foreach (range($number * 2, $limit, $number) as $i) {
+            unset($primes[$i]);
+        }
+        $number = next($primes);
+    }
+
+    return array_values($primes);
+}
