6e135d50bad43f93f4de05cc2cee2d711086388b
Luke Carrier <luke@carrier.im>
2019-08-12 03:09:23 +0100

    php/pig-latin: solve

--- php/pig-latin/pig-latin.php
+++ php/pig-latin/pig-latin.php
@@ -1,1 +1,31 @@
 <?php
+
+function firstVowelPosition(string $word): int
+{
+    preg_match('/\bS?QU/i', $word, $matches);
+    if (count($matches) === 1) {
+        return strlen($matches[0]);
+    }
+
+    preg_match('/\bYT|XR/i', $word, $matches);
+    if (count($matches) === 1) {
+        return 0;
+    }
+
+    return strcspn($word, 'aeiouAEIOU');
+}
+
+function translate(string $text): string
+{
+    $words = explode(' ', $text);
+    foreach ($words as &$word) {
+        $firstVowelPos = firstVowelPosition($word);
+        if ($firstVowelPos !== 0) {
+            $leadingConsonants = substr($word, 0, $firstVowelPos);
+            $word = substr($word, $firstVowelPos) . $leadingConsonants;
+        }
+        $word .= 'ay';
+    }
+
+    return implode(' ', $words);
+}
