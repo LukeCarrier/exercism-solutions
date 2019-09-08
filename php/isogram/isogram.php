23cf2b11e8f0a7e02a8527c724711c54ebc510c4
Luke Carrier <luke@carrier.im>
2019-09-08 18:52:04 +0100

    php/isogram: solve

--- php/isogram/isogram.php
+++ php/isogram/isogram.php
@@ -1,1 +1,9 @@
 <?php
+
+function isIsogram(string $word): bool
+{
+    $word = mb_strtolower(mb_ereg_replace('[\p{P}\p{Z}]', '', $word));
+    $word = preg_split('//u', $word, null, PREG_SPLIT_NO_EMPTY);
+
+    return count($word) === count(array_unique($word));
+}
