0106756be40c90a57f80d28809025b340d74b05a
Luke Carrier <luke@carrier.im>
2019-09-10 00:01:57 +0100

    php/grains: solve it faster with GMP

--- php/grains/grains.php
+++ php/grains/grains.php
@@ -5,10 +5,10 @@ function square(int $square): string
     if ($square < 1 || $square > 64) {
         throw new InvalidArgumentException('$square must be within the range 1-64');
     }
-    return bcpow(2, $square - 1);
+    return gmp_pow(2, $square - 1);
 }
 
 function total(): string
 {
-    return bcsub(bcpow(2, 64), 1);
+    return gmp_init(str_repeat(1, 64), 2);
 }
