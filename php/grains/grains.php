0d4e9cbc0ee77306ef543c7cc7982bc2eb29b16d
Luke Carrier <luke@carrier.im>
2019-09-10 00:01:37 +0100

    php/grains: solve it

--- php/grains/grains.php
+++ php/grains/grains.php
@@ -1,1 +1,14 @@
 <?php
+
+function square(int $square): string
+{
+    if ($square < 1 || $square > 64) {
+        throw new InvalidArgumentException('$square must be within the range 1-64');
+    }
+    return bcpow(2, $square - 1);
+}
+
+function total(): string
+{
+    return bcsub(bcpow(2, 64), 1);
+}
