a45a4c0ff77ae21df10e3a80e12143c3d084c5e0
Luke Carrier <luke@carrier.im>
2019-09-29 21:00:09 +0100

    php/robot-name: solve

--- php/robot-name/robot-name.php
+++ php/robot-name/robot-name.php
@@ -1,1 +1,41 @@
 <?php
+
+class Robot
+{
+    const ALPHA_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const NUM_CHARS = '0123456789';
+
+    protected static $GENERATED_NAMES = [];
+    protected $name;
+
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    protected static function randomAlpha(): string
+    {
+        return substr(static::ALPHA_CHARS, rand(0, strlen(static::ALPHA_CHARS) - 1), 1);
+    }
+
+    protected static function randomNum(): string
+    {
+        return substr(static::NUM_CHARS, rand(0, strlen(static::NUM_CHARS) - 1), 1);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function reset(): void
+    {
+        do {
+            $name = ""
+                    . static::randomAlpha() . static::randomAlpha()
+                    . static::randomNum() . static::randomNum() . static::randomNum();
+        } while (in_array($name, static::$GENERATED_NAMES));
+        static::$GENERATED_NAMES[] = $name;
+        $this->name = $name;
+    }
+}
