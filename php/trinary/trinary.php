aabec20ea03dae92dde64b73d5d1735a9e067e0d
Luke Carrier <luke@carrier.im>
2019-10-09 21:41:06 +0100

    php/trinary: solve

--- php/trinary/trinary.php
+++ php/trinary/trinary.php
@@ -1,1 +1,18 @@
 <?php
+
+function toDecimal(string $number): int
+{
+    $places = str_split(strrev($number));
+    $outOfRange = array_filter($places, function($place) {
+        return $place > 2;
+    });
+    if (count($outOfRange)) {
+        return 0;
+    }
+
+    array_walk($places, function(&$number, $place) {
+        $number = $number * 3 ** $place;
+    });
+
+    return array_sum($places);
+}
