63c769344c8652a19f242731f418881cc6aa9b08
Luke Carrier <luke@carrier.im>
2019-10-09 21:24:11 +0100

    php/flatten-array: solve

--- php/flatten-array/flatten-array.php
+++ php/flatten-array/flatten-array.php
@@ -1,1 +1,13 @@
 <?php
+
+function flatten(array $array): array
+{
+    $result = [];
+    array_walk_recursive($array, function($item) use(&$result) {
+        if ($item !== null) {
+            $result[] = $item;
+        }
+    });
+
+    return $result;
+}
