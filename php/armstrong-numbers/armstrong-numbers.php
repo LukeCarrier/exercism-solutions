757b25a47f577045d72b5261152a56997606bf2c
Luke Carrier <luke@carrier.im>
2019-10-11 13:04:18 +0100

    php/armstrong-numbers: solve

--- php/armstrong-numbers/armstrong-numbers.php
+++ php/armstrong-numbers/armstrong-numbers.php
@@ -1,1 +1,15 @@
 <?php
+
+function isArmstrongNumber(int $number): bool
+{
+    $count = floor(log10($number)) + 1;
+    $sum = 0;
+    $work = $number;
+
+    while ($work > 0) {
+        $sum += ($work % 10) ** $count;
+        $work /= 10;
+    }
+
+    return $number == $sum;
+}
