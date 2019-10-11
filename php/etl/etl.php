131553556d98d27e2f7ed40721750a5385259ef6
Luke Carrier <luke@carrier.im>
2019-10-11 13:12:01 +0100

    php/etl: solve

--- php/etl/etl.php
+++ php/etl/etl.php
@@ -1,1 +1,12 @@
 <?php
+
+function transform(array $scores): array
+{
+    $result = [];
+    foreach ($scores as $score => $letters) {
+        foreach ($letters as $letter) {
+            $result[strtolower($letter)] = $score;
+        }
+    }
+    return $result;
+}
