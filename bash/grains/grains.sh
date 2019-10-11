522b3055e643ba4a73e838c0b745860c1a529ccc
Luke Carrier <luke@carrier.im>
2019-10-11 13:49:38 +0100

    bash/grains: solve

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+square() {
+  local num="$1"
+  if (( $num == 1)); then
+    echo 1
+    return
+  fi
+  echo "2^(${num}-1)" | bc
+}
+total() {
+  result=0
+  for index in {1..64}; do
+    grains="$(square "$index")"
+    result="$(echo "${result}+${grains}" | bc)"
+  done
+  echo $result
+}
+
+if [[ "$1" == 'total' ]]; then
+  total
+elif (( $1 >= 1 && $1 <= 64 )); then
+  square "$1"
+else
+  echo 'Error: invalid input'
+  exit 1
+fi
