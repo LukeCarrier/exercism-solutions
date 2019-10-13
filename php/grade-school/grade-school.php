921a032edb62f3cf168b8ceff1838e29f6fa44bc
Luke Carrier <luke@carrier.im>
2019-10-13 20:42:17 +0100

    php/grade-school: solve

--- php/grade-school/grade-school.php
+++ php/grade-school/grade-school.php
@@ -1,1 +1,33 @@
 <?php
+
+class School
+{
+    private $students = [];
+
+    public function add(string $student, int $grade): void
+    {
+        if (!array_key_exists($grade, $this->students)) {
+            $this->students[$grade] = [];
+        }
+        $this->students[$grade][] = $student;
+    }
+
+    public function grade(int $grade): array
+    {
+        if (!array_key_exists($grade, $this->students)) {
+            return [];
+        }
+        sort($this->students[$grade]);
+        return $this->students[$grade];
+    }
+
+    public function studentsByGradeAlphabetical(): array
+    {
+        ksort($this->students);
+        $result = [];
+        foreach (array_keys($this->students) as $grade) {
+            $result[$grade] = $this->grade($grade);
+        }
+        return $result;
+    }
+}
