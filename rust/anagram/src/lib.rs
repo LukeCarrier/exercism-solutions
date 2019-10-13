2a46c56d4be9e19305a0047c5e492e1b7e06ec25
Luke Carrier <luke@carrier.im>
2019-10-13 20:18:33 +0100

    rust/anagram: solve

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,20 @@
-use std::collections::HashSet;
+use std::collections::{HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn sort_chars(word: &str) -> Vec<char> {
+    let mut result = word
+        .to_lowercase()
+        .chars()
+        .collect::<Vec<_>>();
+    result.sort();
+    result
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a[&str]) -> HashSet<&'a str> {
+    let lower = word.to_lowercase();
+    let sorted = sort_chars(word);
+    possible_anagrams
+        .iter()
+        .filter(|a| a.to_lowercase() != lower && sort_chars(a) == sorted)
+        .map(|a| *a)
+        .collect()
 }
