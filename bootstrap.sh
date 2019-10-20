cc0c50b69e54db6cc9e7e7af9853608f242bf243
Luke Carrier <luke@carrier.im>
2019-10-20 20:13:49 +0100

    .: set style for bootstrap/activate and retab

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,25 +5,25 @@ platform=linux
 arch=64bit
 
 make_url() {
-    echo "https://github.com/exercism/cli/releases/download/v${version}/exercism-${platform}-${arch}.tgz"
+  echo "https://github.com/exercism/cli/releases/download/v${version}/exercism-${platform}-${arch}.tgz"
 }
 
 make_archive_filename() {
-    echo "exercism-${version}-${platform}-${arch}.tgz"
+  echo "exercism-${version}-${platform}-${arch}.tgz"
 }
 
 download() {
-    curl -L -o "$(make_archive_filename)" "$(make_url)"
+  curl -L -o "$(make_archive_filename)" "$(make_url)"
 }
 
 install() {
-    rm -rf _exercism/
-    tar -x -f "$(make_archive_filename)" \
-        --one-top-level=_exercism
+  rm -rf _exercism/
+  tar -x -f "$(make_archive_filename)" \
+      --one-top-level=_exercism
 }
 
 cleanup() {
-    rm -f "$(make_archive_filename)"
+  rm -f "$(make_archive_filename)"
 }
 
 download
