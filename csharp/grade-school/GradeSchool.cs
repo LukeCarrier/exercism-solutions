34960b75c18bcfcc3e2569ddab37f004e44ba3b9
Luke Carrier <luke@carrier.im>
2019-10-22 22:57:40 +0100

    csharp/grade-school: style changes

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -8,14 +8,16 @@ public class GradeSchool
 
     public void Add(string student, int grade)
     {
-        students[student] = grade;
+        students.Add(student, grade);
     }
 
-    public IEnumerable<string> Roster() => from student in students
+    public IEnumerable<string> Roster() =>
+            from student in students
             orderby student.Value, student.Key
             select student.Key;
 
-    public IEnumerable<string> Grade(int grade) => from student in students
+    public IEnumerable<string> Grade(int grade) =>
+            from student in students
             orderby student.Key
             where student.Value == grade
             select student.Key;
