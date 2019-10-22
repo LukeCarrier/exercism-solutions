6d319309260589e53137ef77f8e755caca61076b
Luke Carrier <luke@carrier.im>
2019-10-22 11:35:58 +0100

    bash/grains: simplify total

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
+BOARD_SQUARES=64
+
 square() {
   local num="$1"
   printf '%u\n' "$(( 2 ** ($num - 1) ))"
 }
+
 total() {
-  result=0
-  for index in {1..64}; do
-    grains="$(square "$index")"
-    result="$(bc <<< "${result}+${grains}")"
-  done
-  echo $result
+  printf '%u\n' "$(( 2 ** ($BOARD_SQUARES + 1) - 1 ))"
 }
 
 if [[ "$1" == 'total' ]]; then
   total
-elif (( $1 >= 1 && $1 <= 64 )); then
+elif (( $1 >= 1 && $1 <= $BOARD_SQUARES )); then
   square "$1"
 else
   echo 'Error: invalid input'
